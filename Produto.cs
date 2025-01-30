8d0d309b549e8d7d3c565bc263c1c2f94a8b3276
Leonardo <163869485+LeoMilare@users.noreply.github.com>
2025-01-30 15:51:12 -0300

    Add files via upload

---
+++ Produto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AddProdutos(int quantidade)
+        {
+            Quantidade = Quantidade + quantidade;
+        }
+        public void RemoverProdutos(int remover) 
+        { 
+            Quantidade = Quantidade - remover;
+        }
+        public override string ToString()
+        {
+            return Nome + ", $" + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total: $" + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture) + " Reais.";
+        }
+    }
+}
